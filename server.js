d9bc2c55629ecc29b111c04288d8bdc5a4225a81
Ankit <web.ankitjain@gmail.com>
2020-11-01 10:41:50 +0530

    setup db connection,create menu model and fetch data from db

--- server.js
+++ server.js
@@ -4,15 +4,27 @@ const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3300
+const mongoose = require('mongoose')
+
+// Database connection
+const url = 'mongodb+srv://Ankitpizza:ankit@5555@cluster0.wiltt.mongodb.net/pizza?retryWrites=true&w=majority';
+
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database Connected.....');
+}).catch(err => {
+    console.log('connection failed....')
+});
+
+// Assets
+app.use(express.static('public'))
 
 // set Template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
-
-// Assets
-app.use(express.static('public'))
 
 require('./routes/web')(app)
 
