2362d15b4af1944224a3f3e2180eee5b0af97e7a
Ankit <web.ankitjain@gmail.com>
2020-11-02 16:28:56 +0530

    add session,login,register and logout

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
 
 
 // Database connection
@@ -20,6 +21,7 @@ connection.once('open', () => {
 }).catch(err => {
     console.log('connection failed....')
 });
+
 
 // Session Store
 
@@ -37,15 +39,25 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24}  //24hours
 }))
 
+
+// Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 app.use(flash())
 
 // Assets
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 // Global middleware
 app.use((req,res, next)=> {
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 // set Template engine
